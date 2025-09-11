4691eb2110dbe5cf92b04067e22f29e83176c2aa
Ojetunde Elijah <ojetundeelijah2020@gmail.com>
2025-09-11 12:08:05 -0700

    I implemented a global exception handler

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,7 +1,13 @@
 package com.pm.patientservice.service;
 
+import com.pm.patientservice.dto.PatientRequestDTO;
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.mapper.PatientMapper;
+import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRespository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PatientService {
@@ -11,4 +17,17 @@ public class PatientService {
         this.patientRespository = patientRepository;
     }
 
+    public List<PatientResponseDTO> getPatients(){
+        List<Patient> patients = patientRespository.findAll();
+
+        List<PatientResponseDTO> patientResponseDTOS = patients.stream().map(patient -> PatientMapper.toDTO(patient)).toList();
+
+        return patientResponseDTOS;
+    }
+
+    public  PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO){
+        Patient newPatient = patientRespository.save(PatientMapper.toModel(patientRequestDTO));
+
+        return PatientMapper.toDTO(newPatient);
+    }
 }
